241aa0a2e3df5bbe29b79c1a531b4640a23aeeb5
Ibrahim-AbuShara <ibrahimaymank27@gmail.com>
2021-11-06 18:45:43 +0200

    finch the client

--- Web API  tasks/Scoket_server_client/serve.py
+++ Web API  tasks/Scoket_server_client/serve.py
@@ -1,13 +1,38 @@
 import socket
 import threading
-
+#define constants 
 PORT = 5050
 #SERVR ="192.168.1.5" static
 SERVER = socket.gethostbyname(socket.gethostname()) #quire to get ip addrees dinamcly
-#print(str(SERVER))
-ADDR=(SERVER,PORT) #make a tuple to save the port an the ip
+HEADER = 64 
+FORMAT ="UTF-8"
+ADDR=(SERVER,PORT) #make a tuple to save the port & the ip
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+#---------------------------------------------------------------------
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)#set the type of IP address we look for
 server.bind(ADDR) #to make any deivice that tring to reach the server to hit this socket
 
+def handel_client(conn,addr):
+    print(f'[NEW CONNECTION] {addr} is connected!') #show the ip address of the new client
+    coonnected =True 
+    while coonnected:
+        msg_lengh=conn.recv(HEADER).decode(FORMAT) #recve client massage on 64 bit and decode it on UTF-8
+        msg_lengh=int(msg_lengh) # get the real lenght of client massage
+        msg=conn.recv(msg_lengh).decode(FORMAT) #receve the masge on its fixed lengh
+        if msg == DISCONNECT_MESSAGE:
+            coonnected=False
+        print(f"{addr}:{msg}")
+    conn.close()
+
 def start():
-    pass
+    server.listen() #weting for client acction
+    while True:
+        conn , addr =server.accept() #accept client action and block anther reqests till the prosses compleat
+        thread=threading.Thread(target=handel_client,args=(conn,addr)) #call handel_client method in a thred
+        thread.start()
+
+
+print(f'[STARTING] server {SERVER} is starting..........................')
+start()
+
