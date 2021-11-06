46f78bbf1122d552da6384f85eb473ad7aab0542
Ibrahim-AbuShara <ibrahimaymank27@gmail.com>
2021-11-06 15:30:53 +0200

    add sart

--- Web API  tasks/Scoket_server_client/serve.py
+++ Web API  tasks/Scoket_server_client/serve.py
@@ -3,5 +3,11 @@ import threading
 
 PORT = 5050
 #SERVR ="192.168.1.5" static
-SERVER = socket.gethostbyname(socket.gethostname())
-print(str(SERVER))
+SERVER = socket.gethostbyname(socket.gethostname()) #quire to get ip addrees dinamcly
+#print(str(SERVER))
+ADDR=(SERVER,PORT) #make a tuple to save the port an the ip
+server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)#set the type of IP address we look for
+server.bind(ADDR) #to make any deivice that tring to reach the server to hit this socket
+
+def start():
+    pass
