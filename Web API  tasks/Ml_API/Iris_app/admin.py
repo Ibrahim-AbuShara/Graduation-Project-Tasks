650aaadc5e748748440c737f0c9af7926e44e28f
amiramomen <amiramomen999@gmail.com>
2021-11-17 14:47:46 +0200

    add Iris_App

--- Web API  tasks/Ml_API/Iris_app/admin.py
+++ Web API  tasks/Ml_API/Iris_app/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 '''from .models import  --------
-admin.site.register(jobs)'''
+admin.site.register(model)'''
+
+from .models import Iris_in_model
+
+admin.site.register(Iris_in_model)
--- Web API  tasks/Ml_API/Iris_app/models.py
+++ Web API  tasks/Ml_API/Iris_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Iris_in_model(models.Model):
+    SepalLengthCm=models.FloatField()
+    SepalWidthCm=models.FloatField()
+    PetalLengthCm=models.FloatField()
+    PetalWidthCm=models.FloatField()
+    
